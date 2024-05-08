6089b82677dd45e652518482b6336c039190ef69
Anderson Luiz <anderson.luiz@liferay.com>
2024-05-08 19:38:44 -0300

    refactor + parameterized providers run

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -29,25 +29,36 @@ public class Application {
     private final static Log logger = LogFactory.getLog(Application.class);
     private static HttpClient client = HttpClient.newHttpClient();
 
-    public static void consumeServerSentEvent() throws IOException, InterruptedException {
-        String url = "https://live.tipminer.com/rounds/DOUBLE/644c2d334be055188b0e6237/live";
+    private static String SMASH_PROVIDER = "644c2f2f95e6be2d44a2e277";
+    private static String BETFIERY_PROVIDER = "644c2d334be055188b0e6237";
+
+    private static Map<String, String> providersMap = Map.of(
+    SMASH_PROVIDER, "smash",
+    BETFIERY_PROVIDER, "betfiery"
+);
+
+    public static void consumeServerSentEvent(String providerId) throws IOException, InterruptedException {
+        String url = "https://live.tipminer.com/rounds/DOUBLE/%s/live";
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
+                .uri(URI.create(String.format(url, providerId)))
                 .GET()
                 .timeout(Duration.ofSeconds(120))
                 .build();
 
         Stream<String> linesInResponse = client.send(request, HttpResponse.BodyHandlers.ofLines()).body();
-        linesInResponse.filter( data -> data.contains("data")).map(data -> data.split(": ")[1]).forEach(Application::_save);
+        linesInResponse.filter( data -> data.contains("data")).map(data -> data.split(": ")[1]).forEach(data -> _save(data, providerId));
     }
 
-    private static void _save(String data) {
+    private static void _save(String data, String providerId) {
         try {
+            String url = "https://cassino-database-manager-production.up.railway.app/api/%s/double/save";
+            String platform = providersMap.get(providerId);
+
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://cassino-database-manager-production.up.railway.app/api/betfiery/double/save"))
+                    .uri(URI.create(String.format(url, platform )))
                     .header("Content-Type", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString(_mapRoll(data)))
+                    .POST(HttpRequest.BodyPublishers.ofString(_mapRoll(data, platform)))
                     .timeout(Duration.ofSeconds(10))
                     .build();
 
@@ -60,7 +71,7 @@ public class Application {
         }
     }
 
-    private static String _mapRoll(String data) throws JsonProcessingException {
+    private static String _mapRoll(String data, String platform) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         ArrayNode jsonRoot = (ArrayNode) mapper.readTree(data);
         JsonNode json = jsonRoot.get(0);
@@ -68,12 +79,12 @@ public class Application {
         
         mapped.put("roll", json.get("result").asInt());
         mapped.put("color", _mapColor(json.get("result").asInt()));
-        mapped.put("platform", "betfiery");
+        mapped.put("platform", platform);
         mapped.put("created", json.get("date"));
         mapped.put("total_red_money", 0);
         mapped.put("total_black_money", 0);
         mapped.put("total_white_money", 0);
-        System.out.println(mapped.toString());
+        logger.info(mapped);
 
         return mapped.toString();
     }
@@ -83,6 +94,7 @@ public class Application {
     }
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        consumeServerSentEvent();
+        consumeServerSentEvent(BETFIERY_PROVIDER);
+        // consumeServerSentEvent(SMASH_PROVIDER);
     }
 }
